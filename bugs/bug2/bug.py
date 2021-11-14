b80e07fc8bc77f7fb2d8eb995cc66229de0c7523
0o <2179662317@qq.com>
2021-11-14 23:23:07 +0800

    ok

---
+++ bugs/bug2/bug.py
@@ -0,0 +1,5 @@
+# https://stackoverflow.com/questions/63840447/tensorflow-backend-bug-in-model-make-predict-function
+
+# model = deepmoji_emojis(maxlen, PRETRAINED_PATH)
+# model.summary()
+# model._make_predict_function()
