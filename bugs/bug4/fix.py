cffa741a36c76cdc41ebfa0416fd406168d096dd
No-edge <61647929+No-edge@users.noreply.github.com>
2021-11-15 23:25:04 +0800

    Create fix.py

---
+++ bugs/bug4/fix.py
@@ -0,0 +1,40 @@
+# Load the TensorBoard notebook extension
+%load_ext tensorboard
+
+import tensorflow as tf
+print(tf.__version__)
+import datetime, os
+
+fashion_mnist = tf.keras.datasets.fashion_mnist
+
+(x_train, y_train),(x_test, y_test) = fashion_mnist.load_data()
+x_train, x_test = x_train / 255.0, x_test / 255.0
+
+def create_model():
+  return tf.keras.models.Sequential([
+    tf.keras.layers.Flatten(input_shape=(28, 28)),
+    tf.keras.layers.Dense(512, activation='relu'),
+    tf.keras.layers.Dropout(0.2),
+    tf.keras.layers.Dense(10, activation='softmax')
+  ])
+
+def train_model():
+
+  model = create_model()
+  model.compile(optimizer='adam',
+                loss='sparse_categorical_crossentropy',
+                metrics=['accuracy'])
+
+  #NAME = "Trains_vs_Cars_16by2_CNN_{}".format(int(time.time()))
+  NAME = "Trains_vs_Cars_16by2_{}".format(str(datetime.datetime.now()))
+  tensorboard = tf.keras.callbacks.TensorBoard(log_dir="logs/{}".format(NAME))
+
+  model.fit(x=x_train, 
+            y=y_train, 
+            batch_size=25,
+            epochs=5, 
+            # validation_split=0.2,
+            validation_data=(x_test, y_test), 
+            callbacks=[tensorboard])
+
+train_model()
